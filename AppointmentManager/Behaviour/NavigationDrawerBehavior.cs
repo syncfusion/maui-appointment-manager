7f35fa55a713d60bb7172c0042a32ae6bf263706
SriRadheshNag <sriradheshnag.s@syncfusion.com>
2025-03-11 10:32:19 +0530

    updated

--- AppointmentManager/Behaviour/NavigationDrawerBehavior.cs
+++ AppointmentManager/Behaviour/NavigationDrawerBehavior.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.Maui.Controls;
-using Syncfusion.Maui.NavigationDrawer;
+using Syncfusion.Maui.Toolkit.NavigationDrawer;
 
 namespace ManageAppointments.Behaviors
 {
